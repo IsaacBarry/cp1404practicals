31ee0e636687c837f83b226a87905d97d56e9ba2
Isaac 2.0 <isaac.barry@my.jcu.edu.au>
2021-03-21 09:54:30 +1000

    Added fahrenheit to celsius function

--- Practical 3/temperatures.py
+++ Practical 3/temperatures.py
@@ -6,6 +6,14 @@ Pseudocode for temperature conversion
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
+
+
+def fahrenheit_to_celsius():
+    fahrenheit = float(input("Fahrenheit:"))
+    # celsius = 5 / 9 * (fahrenheit - 32)
+    return 5/9 * (fahrenheit - 32)
+
+
 print(MENU)
 choice = input(">>> ").upper()
 while choice != "Q":
@@ -14,10 +22,7 @@ while choice != "Q":
         fahrenheit = celsius * 9.0 / 5 + 32
         print("Result: {:.2f} F".format(fahrenheit))
     elif choice == "F":
-        # Hint: celsius = 5 / 9 * (fahrenheit - 32)
-        # Remove the "pass" statement when you are done. It's a placeholder.
-        fahrenheit = float(input("Fahrenheit:"))
-        celsius = 5 / 9 * (fahrenheit - 32)
+        celsius = fahrenheit_to_celsius()
         print("Result: {:.2f} C".format(celsius))
     else:
         print("Invalid option")
