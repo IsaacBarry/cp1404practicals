98451b84d9dc43d2dae33dd2f3e613b95bc2e5df
Isaac 2.0 <isaac.barry@my.jcu.edu.au>
2021-03-21 09:59:49 +1000

    Added main function

--- Practical 3/temperatures.py
+++ Practical 3/temperatures.py
@@ -8,6 +8,23 @@ F - Convert Fahrenheit to Celsius
 Q - Quit"""
 
 
+def main():
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            fahrenheit = celsius_to_fahrenheit()
+            print("Result: {:.2f} F".format(fahrenheit))
+        elif choice == "F":
+            celsius = fahrenheit_to_celsius()
+            print("Result: {:.2f} C".format(celsius))
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
 def fahrenheit_to_celsius():
     fahrenheit = float(input("Fahrenheit:"))
     # celsius = 5 / 9 * (fahrenheit - 32)
@@ -19,17 +36,4 @@ def celsius_to_fahrenheit():
     return celsius * 9.0/5 +32
 
 
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        fahrenheit = celsius_to_fahrenheit()
-        print("Result: {:.2f} F".format(fahrenheit))
-    elif choice == "F":
-        celsius = fahrenheit_to_celsius()
-        print("Result: {:.2f} C".format(celsius))
-    else:
-        print("Invalid option")
-    print(MENU)
-    choice = input(">>> ").upper()
-print("Thank you.")
+main()
