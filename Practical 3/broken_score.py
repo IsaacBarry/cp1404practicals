08ba29b9ace3620270818d05e819503c07623439
Isaac 2.0 <isaac.barry@my.jcu.edu.au>
2021-03-21 10:07:15 +1000

    Added main function

--- Practical 3/broken_score.py
+++ Practical 3/broken_score.py
@@ -3,12 +3,17 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-if 0 > score or score > 100:
-    print("Invalid Score")
-elif score < 50:
-    print("Bad")
-elif score < 90:
-    print("Pass")
-elif score <= 100:
-    print("Excellent")
+
+def main():
+    score = float(input("Enter score: "))
+    if 0 > score or score > 100:
+        print("Invalid Score")
+    elif score < 50:
+        print("Bad")
+    elif score < 90:
+        print("Pass")
+    elif score <= 100:
+        print("Excellent")
+
+
+main()
