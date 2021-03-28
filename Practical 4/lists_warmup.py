62e2cb76daf975a84f49fb295a6c10cbae2454f0
Isaac 2.0 <isaac.barry@my.jcu.edu.au>
2021-03-28 14:27:48 +1000

    Answered all 4 questions

--- Practical 4/lists_warmup.py
+++ Practical 4/lists_warmup.py
@@ -20,3 +20,11 @@ numbers[3:4]
 numbers + [6, 5, 3]
 # 3 1 4 1 5 9 2 6 5 3
 
+# Change first number to 'ten'
+numbers[0] = 'ten'
+# change last number to 1
+numbers[-1] = 1
+# get all numbers beside first 2
+numbers[2:]
+# check if 9 in numbers
+9 in numbers
